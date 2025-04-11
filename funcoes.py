ca67e23f08c3758f067a073e466cd28513557bb1
Leo <leonardo.sobrinho864@gmail.com>
2025-04-11 13:22:23 -0300

    Primeiro commit

---
+++ funcoes.py
@@ -0,0 +1,35 @@
+
+
+def adicionar_tarefa(tarefas):
+    nova_tarefa = str(input("Digite a nova tarefa: "))
+    tarefas.append(nova_tarefa)
+    print("Tarefa adicionada com sucesso!")
+
+
+
+def listar_tarefas(tarefas):
+    if not tarefas:
+        print("Nenhuma tarefa cadastrada.")
+    else:
+        for i, tarefa in enumerate(tarefas, start=1):
+            print(f"{i}. {tarefa}")
+
+
+
+def concluir_tarefa(tarefas):
+    if not tarefas:
+        print("Nenhuma tarefa cadastrada.")
+        return
+    
+    listar_tarefas(tarefas)
+    
+    try:
+        indice_tarefa = int(input("Digite o índice da tarefa concluída: "))
+        if "✔" not in tarefas[indice_tarefa - 1]:
+            tarefas[indice_tarefa - 1] += " ✔"
+            print("Tarefa marcada como concluída!")
+        else:
+            print("Essa tarefa já está marcada como concluída.")
+    
+    except (ValueError, IndexError):
+        print("Índice inválido. Tente novamente.")
---
+++ main.py
@@ -0,0 +1,35 @@
+import sys
+from funcoes import * 
+tarefas = []
+
+def main():
+    print("GERENCIADOR DE TAREFAS")
+    
+    #Inicia o loop principal e pede o input do usuario.
+    while True:
+        opcao = str(input('''[1] Adicionar tarefa
+[2] Listar Tarefas
+[3] Marcar tarefa como concluída
+[4] Sair
+Selecione uma opção: '''))
+        
+        match opcao:
+            case "1":
+                adicionar_tarefa(tarefas)
+            
+            case "2":
+                listar_tarefas(tarefas)
+            
+            case "3":
+                concluir_tarefa(tarefas)
+            
+            case "4":
+                sys.exit("Encerrando aplicação...")
+            
+            case _:
+                print("Digite uma opção válida.")
+
+
+
+if __name__ == "__main__":
+    main()
